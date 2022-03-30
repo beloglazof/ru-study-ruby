6aeeb26eed7888d5615b6fdc8082e092f6ea1229
Slava <v.beloglazov@rambler-co.ru>
2022-03-30 12:08:56 +0300

    Add Arrays.search method

--- test/exercise/arrays/solution.rb
+++ test/exercise/arrays/solution.rb
@@ -13,7 +13,32 @@ module Exercise
       end
 
       def search(_array, _query)
-        0
+        guess_iter = lambda { |min, max|
+          return -1 if max == 0
+
+          slice_length = max + min
+          guess = (slice_length / 2.0).floor
+
+          if slice_length <= 2 
+            if _array[guess] == _query
+              return guess
+            elsif _array[guess - 1] == _query
+              return guess - 1
+            else
+              return -1
+            end
+          end
+          
+          if _array[guess] == _query
+            return guess
+          elsif _array[guess] > _query
+            guess_iter.call(min, guess - 1)
+          else
+            guess_iter.call(guess + 1, max)
+          end
+        }
+      
+        guess_iter.call(0, _array.length)
       end
     end
   end
